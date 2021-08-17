f20579b7ef4f0f2a894961032f5f3e36304daf13
luciussulla <maciek.kowalczyk@yandex.com>
2021-08-17 20:17:55 +0200

    Followers tab

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { GithubContext } from '../contexts/context'
+
 
 export default function Followers() {
+  const {followers} = React.useContext(GithubContext)
+
   return (
     <div className="followers">
-      
+        {followers.map((follower, idx)=> {
+          const {avatar_url: img, html_url, login} = follower
+          return <article key={idx}>
+            <img src={img} alt={login} />
+            <div>
+              <h4>{login}</h4>
+              <a href={html_url}>{html_url}</a>
+            </div>
+          </article>
+        })}
     </div>
   )
 }
