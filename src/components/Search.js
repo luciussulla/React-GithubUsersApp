5e11b699c184d1dcff60537586796ad698ffc761
luciussulla <maciek.kowalczyk@yandex.com>
2021-09-15 15:18:20 +0200

    Send axios request to github and get the user. Insert user values into the user tab

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,28 +4,31 @@ import {GithubContext} from '../contexts/context'
 
 const Search = () => {
   const [user, setUser] = React.useState('')
-  // get things from flobal context
+  const {requests, searchGithubUser} = React.useContext(GithubContext)
+  const {error: {msg}} = React.useContext(GithubContext)
+  const {error: {show}} = React.useContext(GithubContext)
+  // const {error} = React.useContext(GithubContext);
+   // get things from flobal context
 
   const handleSubmit = (e)=> {
     e.preventDefault()
-    console.log(user)
     // more loginc soon
     if(user) {
-      
-      
+      searchGithubUser(user)
     }
   }
 
   return (
     <div className="search">
        <form onSubmit={handleSubmit}>
+           {show && <p>{msg}</p>}
           <div className="form-control">
             <MdSearch className="spyglass_icon"/>
             <input type="text" placeholder='enter github user' value={user} onChange={(e)=> setUser(e.target.value)}/>
-            <button type="submit">search</button>
+            { requests > 0 && <button type="submit">search</button>}
           </div>
         </form> 
-        <h3>requests: 60/60 </h3>
+        <h3>requests: {requests}/60 </h3>
     </div>
   )
 }
--- src/contexts/context.js
+++ src/contexts/context.js
@@ -4,7 +4,7 @@ import MockFollowers from './mockData/mockFollowers'
 import MockRepos from './mockData/mockRepos'
 import axios from 'axios'
 
-const url = 'http://api.github.com'
+const rootUrl = 'http://api.github.com'
 
 const GithubContext = createContext()
 
@@ -12,13 +12,56 @@ const GithubProvider = ({children})=> {
   const [githubUser, setGithubUser] = useState(MockUser) 
   const [followers, setFollowers]   = useState(MockFollowers)
   const [repos, setRepos]           = useState(MockRepos)
+  // request loading
+  const [requests, setRequests] = useState(0)
+  const [loading, setIsLoading] = useState(false)
+  // error 
+  const [error, setError] = useState({show: false, msg: ''})
 
+  const searchGithubUser = async(user) => {
+    toggleError() // here we clear the error with the default values in the toggleE...
+    console.log("search user")
+    // toggle error
+    // set Loading 
+    const response = await axios(`${rootUrl}/users/${user}`).
+    catch(err => console.log(err))
+    console.log(response)
+    if(response){
+      setGithubUser(response.data)
+    } else {
+      toggleError(true, "there are users with that username") 
+    }
+  }
+
+  const checkRequests = ()=> {
+    axios(`${rootUrl}/rate_limit`).then(data=> {
+      // console.log(data)
+      let {data: {rate: {remaining}}} = data
+
+      setRequests(remaining)
+      if(remaining ===0) {
+         toggleError(true, 'sorry, you have exceeded you hourly rate limig')
+      }
+    }).catch(e=> {
+      console.log(e)
+    })
+  }
+
+  function toggleError(show = false, msg = '') {
+    setError({show,msg})
+  }
+
+  useEffect(checkRequests,[])
+  
  return  <GithubContext.Provider
           value={
             {
               githubUser, 
               followers, 
               repos,
+              requests, 
+              error, 
+              searchGithubUser, 
             }
           }>
           {children}
